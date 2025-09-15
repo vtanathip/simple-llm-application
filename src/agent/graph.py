9424bf55712c3a26abc3b1e312e3920765608635
LABTOP-VTANATHIP <tanathip.se@gmail.com>
2025-09-15 21:00:11 +0700

    Change example of custom agent workflow to prebuilt agent so it will help visualize a simple concept

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,53 +1,23 @@
-"""LangGraph single-node graph template.
-
-Returns a predefined response. Replace logic and configuration as needed.
+"""Simple agent graph example using LangGraph Prebuilt components.
 """
 
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import Any, Dict, TypedDict
+from langgraph.prebuilt import create_react_agent
 
-from langgraph.graph import StateGraph
-from langgraph.runtime import Runtime
+# Tool for Agent
+def get_weather(city: str) -> str:  
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
 
-
-class Context(TypedDict):
-    """Context parameters for the agent.
-
-    Set these when creating assistants OR when invoking the graph.
-    See: https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/
-    """
-
-    my_configurable_param: str
-
-
-@dataclass
-class State:
-    """Input state for the agent.
-
-    Defines the initial structure of incoming data.
-    See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
-    """
-
-    changeme: str = "example"
-
-
-async def call_model(state: State, runtime: Runtime[Context]) -> Dict[str, Any]:
-    """Process input and returns output.
-
-    Can use runtime context to alter behavior.
-    """
-    return {
-        "changeme": "output from call_model. "
-        f"Configured with {runtime.context.get('my_configurable_param')}"
-    }
-
+# Create Agent
+agent = create_react_agent(
+    model="ollama:llama3.2",  
+    tools=[get_weather],  
+    prompt="You are a helpful assistant"  
+)
 
 # Define the graph
 graph = (
-    StateGraph(State, context_schema=Context)
-    .add_node(call_model)
-    .add_edge("__start__", "call_model")
-    .compile(name="New Graph")
+    agent
 )
