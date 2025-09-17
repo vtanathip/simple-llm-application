5b31e74f00dbb2b1a6cc4432443c1488e681e3d3
LABTOP-VTANATHIP <tanathip.se@gmail.com>
2025-09-18 00:06:16 +0700

    Updated open webui pipelines

--- src/pipeline/langgraph.py
+++ src/pipeline/langgraph.py
@@ -1,16 +1,23 @@
 import requests
-from typing import List, Union, Generator, Iterator
-from pydantic import BaseModel
+import os
+from typing import List, Union, Generator, Iterator, Optional
+from pydantic import BaseModel, Field
 
 
 class Pipeline:
-
     class Valves(BaseModel):
-        pass
+        API_URL: Optional[str] = Field(
+            default="http://host.docker.internal:8000/generate",
+            description="Langgraph API URL"
+        )
 
     def __init__(self):
         self.id = "LangGraph Agent"
         self.name = "LangGraph Agent"
+        # Initialize valve paramaters
+        self.valves = self.Valves(
+            **{k: os.getenv(k, v.default) for k, v in self.Valves.model_fields.items()}
+        )
 
     async def on_startup(self):
         print(f"on_startup: {__name__}")
@@ -24,13 +31,18 @@ class Pipeline:
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
 
-        url = 'http://localhost:8510/openwebui-pipelines/api'
+        last_user_message = messages[-1]["content"]
+        url = self.valves.API_URL
+        if not url:
+            raise ValueError(
+                "API_URL is not configured. Please set it in the pipeline settings.")
+
         headers = {
             'accept': 'application/json',
             'Content-Type': 'application/json'
         }
         data = {
-            "messages": [[msg['role'], msg['content']] for msg in messages]
+            "messages": last_user_message
         }
 
         response = requests.post(url, json=data, headers=headers, stream=True)
